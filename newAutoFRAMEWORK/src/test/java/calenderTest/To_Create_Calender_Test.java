619519d96807f7727bf489d1ccd762d6f6920bd2
md burhan <md burhan@DESKTOP-3JFJOQU>
2024-11-07 15:21:59 +0530

    I Have Added the Calender and Leads Test

---
+++ newAutoFRAMEWORK/src/test/java/calenderTest/To_Create_Calender_Test.java
@@ -0,0 +1,10 @@
+package calenderTest;
+
+public class To_Create_Calender_Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is to the Calender Test");
+	}
+
+}
---
+++ newAutoFRAMEWORK/src/test/java/leadsTest/To_Create_Leads_Tests.java
@@ -0,0 +1,11 @@
+package leadsTest;
+
+public class To_Create_Leads_Tests {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is to Create Leads in this");
+
+	}
+
+}
