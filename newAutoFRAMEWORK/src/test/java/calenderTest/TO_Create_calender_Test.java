581e7028265f4897ac4e991d8b416aa4721ae7c8
md burhan <md burhan@DESKTOP-3JFJOQU>
2024-11-07 15:48:35 +0530

    i Have written testScripts in Calender and Leads Please Check it

---
+++ newAutoFRAMEWORK/src/test/java/calenderTest/TO_Create_calender_Test.java
@@ -0,0 +1,10 @@
+package calenderTest;
+
+public class TO_Create_calender_Test {
+
+	public static void main(String[] args) {
+
+System.out.println("this is the Clender Test");
+	}
+
+}
---
+++ newAutoFRAMEWORK/src/test/java/leadsTest/To_Create_Leads_Test.java
@@ -0,0 +1,10 @@
+package leadsTest;
+
+public class To_Create_Leads_Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("this is to Create Leads Test");
+	}
+
+}
